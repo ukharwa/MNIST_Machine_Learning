724706bf6e77652fbfdcd9f4d6ea7aab44e1782b
ukharwa <umar.kharwa@gmail.com>
2024-09-28 00:18:09 +0200

    Initial Commit - Everything Working as expected

---
+++ src/load_data.py
@@ -0,0 +1,32 @@
+
+def convert(imgs, labels, outfile, n):  #converts from ubyte to csv
+    imgf = open(imgs, "rb")
+    labelf = open(labels, "rb")
+    csvf = open(outfile, "w")
+
+    imgf.read(16)
+    labelf.read(8)
+    images = []
+
+    for i in range(n):
+        image = [ord(labelf.read(1))]
+        for j in range(28*28):
+            image.append(ord(imgf.read(1)))
+        images.append(image)
+
+    for image in images:
+        csvf.write(",".join(str(pix) for pix in image) + "\n")
+    
+    imgf.close()
+    labelf.close()
+    csvf.close()
+
+
+mnist_train_x = "home/ukharwa/MNIST/data/train-images-idx3-ubyte"
+mnist_train_y = "/home/ukharwa/MNIST/data/train-labels-idx1-ubyte"
+
+mnist_test_x = "/home/ukharwa/MNIST/data/t10k-images-idx3-ubyte"
+mnist_test_y = "/home/ukharwa/MNIST/data/t10k-labels-idx1-ubyte"
+
+convert(mnist_train_x, mnist_train_y, "/home/ukharwa/MNIST/data/train.csv", 60000)
+convert(mnist_test_x, mnist_test_y, "/home/ukharwa/MNIST/data/test.csv", 10000)
---
+++ src/model.py
@@ -0,0 +1,31 @@
+
+from DNN_class import DNN
+import numpy as np
+
+# create lists of the training and testing data
+train_file = open("/home/ukharwa/MNIST/data/train.csv", "r")
+train_list = train_file.readlines()
+train_file.close()
+test_file = open("/home/ukharwa/MNIST/data/test.csv", "r")
+test_list = test_file.readlines()
+test_file.close()
+
+weights_file = open("/home/ukharwa/MNIST/weights.txt", "r")
+weight_lines = weights_file.readlines()
+weights_file.close()
+
+arr_weights = []
+for line in weight_lines:
+    arr_weights.append(list(map(float, line.strip().split(","))))
+
+w1 = np.asarray(arr_weights[0]).reshape(128,784)
+w2 = np.asarray(arr_weights[1]).reshape(64,128)
+w3 = np.asarray(arr_weights[2]).reshape(10,64)
+acc = arr_weights[3]
+
+starting_weights = [w1, w2, w3, acc]
+
+
+dnn = DNN(starting_weights)
+dnn.train(train_list, test_list, 10, 0.5)
+#print(dnn.compute_accuracy(test_list) * 100)
