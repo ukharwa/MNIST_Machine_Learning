0b097f61bf9d40a73dab7339b2786ab1e4d8056d
ukharwa <umar.kharwa@gmail.com>
2024-10-08 23:53:23 +0200

    data formatted into x y format

--- src/load_data.py
+++ src/load_data.py
@@ -1,19 +1,23 @@
 import os;
 
-def convert(imgs, labels, outfile, n):  #converts from ubyte to csv
+def convert(imgs, labels, labels_outfile, data_outfile, n):  #converts from ubyte to csv
     imgf = open(imgs, "rb")
     labelf = open(labels, "rb")
-    csvf = open(outfile, "w")
+    csvf = open(data_outfile, "w")
+    lcsvf = open(labels_outfile, "w")
 
     imgf.read(16)
     labelf.read(8)
     images = []
 
     for i in range(n):
-        image = [ord(labelf.read(1))]
+        image = []
         for j in range(28*28):
             image.append(ord(imgf.read(1)))
         images.append(image)
+
+    for i in range(n):
+        lcsvf.write(str(ord(labelf.read(1))) + "\n")
 
     for image in images:
         csvf.write(",".join(str(pix) for pix in image) + "\n")
@@ -21,6 +25,7 @@ def convert(imgs, labels, outfile, n):  #converts from ubyte to csv
     imgf.close()
     labelf.close()
     csvf.close()
+    lcsvf.close()
 
 
 mnist_train_x = os.getcwd() + "/data/train-images-idx3-ubyte"
@@ -29,5 +34,5 @@ mnist_train_y = os.getcwd() + "/data/train-labels-idx1-ubyte"
 mnist_test_x = os.getcwd() + "/data/t10k-images-idx3-ubyte"
 mnist_test_y = os.getcwd() + "/data/t10k-labels-idx1-ubyte"
 
-convert(mnist_train_x, mnist_train_y, os.getcwd() + "/data/train.csv", 60000)
-convert(mnist_test_x, mnist_test_y, os.getcwd() + "/data/test.csv", 10000)
+#convert(mnist_train_x, mnist_train_y, os.getcwd() + "/data/Y_train.csv", os.getcwd() + "/data/X_train.csv", 60000)
+convert(mnist_test_x, mnist_test_y, os.getcwd() + "/data/Y_test.csv", os.getcwd() + "/data/X_test.csv", 10000)
